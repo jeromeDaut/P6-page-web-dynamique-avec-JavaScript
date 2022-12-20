2e0245efb8791b38f82270e3688c826430f39df3
Jérôme Dautreme <jerome.dautreme@live.fr>
2022-12-20 21:03:01 +0100

    ajout  des fonctions pour trier les photos

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,38 +1,38 @@
 
   let categoryData =[];
   let worksData = [];
+  let currentCat = 0;
+  
   const category = document.getElementById("category");
   const gallery = document.querySelector(".gallery");
+  
+  // --------------------------------------------------------
   /* functions asynchronous to get the api data
-  in the table categoryData and worksData: 
-  --------------------------------------------------------*/
-  const fetchData = async()=>{
+  in the table categoryData and worksData: */
+  const fetchCategory = async()=>{
     await fetch("http://localhost:5678/api/categories")
     .then((res) => res.json())
     .then((data) =>(categoryData = data))
-    
-    
+    .catch((err)=>console.log(err,"fetch error "))
     await fetch("http://localhost:5678/api/works")
     .then((res) => res.json())
     .then((data) =>(worksData = data),console.log('fetch valide'))
-    
     .catch((err)=>console.log(err,"fetch error "))
-    
   }
   // --------------------------------------------------
   // display section portfolio :
   const worksDisplay = async () => {
-    await fetchData()
+    
+    await fetchCategory();
     
     // buttons injection in nav.category:
     // -----------------------------------------------
-    category.innerHTML += `
-    <button id=""class="btn ${categoryData.length} active">Tous</button>
-    <button id="" class="btn ${categoryData[0].id} ${categoryData[0].name}">Objets</button>
-    <button id="" class="btn apartment ${categoryData[1].id} ${categoryData[1].name}">Appartements</button>
-    <button id="" class="btn hotel ${categoryData[2].id} ${categoryData[2].name}" >Hôtels et restaurants</button>
-    `;
-    // ----------------------------------------------
+    category.innerHTML = `<button  onclick="currentCat = 0" class="btn active">tous</button> `;
+    category.innerHTML += categoryData.map((cat)=> `
+    <button  onclick="currentCat = ${cat.id}"class="btn ">${cat.name}</button> 
+    `
+    ).join("");
+    // -------------------------------------------------------------------
     // images injection in div.gallery:
     gallery.innerHTML= worksData.map((works) =>  `
     <figure  class="${works.categoryId} ${works.category.name} ">
@@ -41,48 +41,50 @@
     </figure>`
     ).join(""); /* delete the commas created by .map*/
     
-    // buttons 
+    // -------------------------------------------------------------------
+    // buttons select
     const allBtnSelected = category.getElementsByTagName('button');
-    const btnAll = category.getElementsByClassName('btn')[0];
-    const btnObject = category.getElementsByClassName('btn')[1];
-    const btnApartment = category.getElementsByClassName('btn')[2];
-    const btnHotel = category.getElementsByClassName('btn')[3];
-    // categoryId recovery :
     
-      for (let i= 0; i < allBtnSelected.length; i++) {
-          allBtnSelected[i].addEventListener('click',()=>{
-            if (i === 0) {
-            allBtnSelected[i].classList.toggle("active")
-            gallery.innerHTML= worksData.map((works) =>  `
-            <figure  class="${works.category.name} ${works.categoryId}">
-            <img crossorigin="anonymous" src="${works.imageUrl}" alt="${works.title}">
-            <figcaption>${works.title}</figcaption>
-            </figure>`
-            ).join(""); /* delete the commas created by .map*/
-            }else
-            allBtnSelected[i].className.replace('active',"");
-          })
-      };
-      
-      // btnObject.addEventListener('click', ()=>{
-        //   btnObject.classList.toggle('active');
-        //   categoryId.innerHTML= worksData.map((works) =>  `
-        //   <figure  class="${works.categoryId}">
-        //   <img crossorigin="anonymous" src="${works.imageUrl}" alt="${works.title}">
-        //   <figcaption>${works.title}</figcaption>
-        //   </figure>`
-        //   ).join(""); /* delete the commas created by .map*/
-        // })
-        // btnApartment.addEventListener('click', ()=>{})
-        // btnHotel.addEventListener('click', ()=>{})
-        
-        
-        
-        
-        
-      }
-      worksDisplay();
-      
-      
-      
+    for (let i= 0; i < allBtnSelected.length; i++) {
+      // event btns
+      allBtnSelected[i].addEventListener('click',()=>{
+        // ----------------------------------------------------------------
+        // colorize buttons on click
+        for (let i = 0; i < allBtnSelected.length; i++) {
+          allBtnSelected[i].addEventListener("click", function () {
+            let btnClicked = document.getElementsByClassName("active");
+            btnClicked[0].className = btnClicked[0].className.replace("active" ,"");
+            this.className += " active";
+          });
+        };
+        // ----------------------------------------------------------------
+        //condition for display figure
+        if (i === 0) {
+          gallery.innerHTML= worksData.map((works) =>  `
+          <figure class="${works.category.name} ${works.categoryId}">
+          <img crossorigin="anonymous" src="${works.imageUrl}" alt="${works.title}">
+          <figcaption>${works.title}</figcaption>
+          </figure>`
+          ).join("");
+        }else{
+          gallery.innerHTML= "";
+          for(let works of worksData) {
+            if (currentCat === works.categoryId) {
+              console.log(i);
+              gallery.innerHTML +=` 
+              <figure  class="${works.category.name} ${works.categoryId}">
+              <img crossorigin="anonymous" src="${works.imageUrl}" alt="${works.title}">
+              <figcaption>${works.title}</figcaption>
+              </figure>`
+            }
+          }
+        };
+      })
+    };
+  }
+  worksDisplay();
+  
+  
+  
+  
       
