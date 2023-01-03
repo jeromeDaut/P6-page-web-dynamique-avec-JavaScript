a2549eca1d4508d0a740e8ee781c69312867c522
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-01-03 10:54:58 +0100

    ajout/suppression barre admin au log administrateur

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,107 +1,122 @@
+let categoryData = [];
+let worksData = [];
+let currentCat = 0;
+let modal = null;
 
-  let categoryData =[];
-  let worksData = [];
-  let userId=[];
-  let currentCat = 0;
-  let modal= null;
+const category = document.getElementById("category");
+const gallery = document.querySelector(".gallery");
 
-  const category = document.getElementById("category");
-  const gallery = document.querySelector(".gallery");
-  // const galleryMini = document.querySelector('.gallery-mini');
-  
-  logout = ()=>{
-    localStorage.removeItem('info');
-    localation.href('/');
-  }
-  // logout.addEventListener('click', ()=>{
-  //   // console.log(log);
-  // })
-  // --------------------------------------------------------------------
-  /* functions asynchronous to get the api data
+logout = () => {
+  localStorage.removeItem("info");
+  location.href = "/FrontEnd/";
+};
+
+// const fetchLog = async () => {
+//   await fetch("");
+// };
+
+// --------------------------------------------------------------------
+/* functions asynchronous to get the api data
   in the table categoryData and worksData: */
-  const fetchCategory = async()=>{
-    await fetch("http://localhost:5678/api/categories")
+const fetchCategory = async () => {
+  await fetch("http://localhost:5678/api/categories")
     .then((res) => res.json())
-    .then((data) =>(categoryData = data))
-    .catch((err)=>console.log(err,"fetch error "))
-    await fetch("http://localhost:5678/api/works")
+    .then((data) => (categoryData = data))
+    .catch((err) => console.log(err, "fetch error "));
+  await fetch("http://localhost:5678/api/works")
     .then((res) => res.json())
-    .then((data) =>(worksData = data),console.log('fetch valide'))
-    .catch((err)=>console.log(err,"fetch error "))
-  }
-  // ---------------------------------------------------------------------
-  // display section portfolio :
-  const worksDisplay = async () => {
-    await fetchCategory();
-    
-    // -------------------------------------------------------------------
-    // buttons injection in nav.category:
-    category.innerHTML = `<button  onclick="currentCat = 0" class="btn active">Tous</button> `;
-    category.innerHTML += categoryData.map((cat)=> `
+    .then((data) => (worksData = data), console.log("fetch valide"))
+    .catch((err) => console.log(err, "fetch error "));
+};
+// ---------------------------------------------------------------------
+// display section portfolio :
+const worksDisplay = async () => {
+  await fetchCategory();
+
+  // -------------------------------------------------------------------
+  // buttons injection in nav.category:
+  category.innerHTML = `<button  onclick="currentCat = 0" class="btn active">Tous</button> `;
+  category.innerHTML += categoryData
+    .map(
+      (cat) => `
     <button  onclick="currentCat = ${cat.id}"class="btn ">${cat.name}</button> 
     `
-    ).join("");
-    // -------------------------------------------------------------------
-    // images injection in div.gallery:
-    gallery.innerHTML= worksData.map((works) =>  `
+    )
+    .join("");
+  // -------------------------------------------------------------------
+  // images injection in div.gallery:
+  gallery.innerHTML = worksData
+    .map(
+      (works) => `
     <figure  class="${works.categoryId} ${works.category.name} ">
     <img crossorigin="anonymous" src="${works.imageUrl}" alt="${works.title}">
     <figcaption>${works.title}</figcaption>
     </figure>`
-    ).join(""); /* delete the commas created by .map*/
-    
-    // -------------------------------------------------------------------
-    // buttons select
-    const allBtnSelected = category.getElementsByTagName('button');
-    
-    for (let i= 0; i < allBtnSelected.length; i++) {
-      // event btns
-      allBtnSelected[i].addEventListener('click',()=>{
-        // colorize buttons on click
-        for (let j = 0; j < allBtnSelected.length; j++) {
-          allBtnSelected[j].addEventListener("click", function () {
-            let btnClicked = document.getElementsByClassName("active");
-            btnClicked[0].className = btnClicked[0].className.replace("active" ,"");
-            this.className += " active";
-          });
-        };
-        // ----------------------------------------------------------------
-        //condition for display figure
-        if (i === 0) {
-          gallery.innerHTML= worksData.map((works) =>  `
+    )
+    .join(""); /* delete the commas created by .map*/
+
+  // -------------------------------------------------------------------
+  // buttons select
+  const allBtnSelected = category.getElementsByTagName("button");
+
+  for (let i = 0; i < allBtnSelected.length; i++) {
+    // event btns
+    allBtnSelected[i].addEventListener("click", () => {
+      // colorize buttons on click
+      for (let j = 0; j < allBtnSelected.length; j++) {
+        allBtnSelected[j].addEventListener("click", function () {
+          let btnClicked = document.getElementsByClassName("active");
+          btnClicked[0].className = btnClicked[0].className.replace(
+            "active",
+            ""
+          );
+          this.className += " active";
+        });
+      }
+      // ----------------------------------------------------------------
+      //condition for display figure
+      if (i === 0) {
+        gallery.innerHTML = worksData
+          .map(
+            (works) => `
           <figure class="${works.category.name} ${works.categoryId}">
           <img crossorigin="anonymous" src="${works.imageUrl}" alt="${works.title}">
           <figcaption>${works.title}</figcaption>
           </figure>`
-          ).join("");
-        }else{
-          gallery.innerHTML= "";
-          for(let works of worksData) {
-            if (currentCat === works.categoryId) {
-              gallery.innerHTML +=` 
+          )
+          .join("");
+      } else {
+        gallery.innerHTML = "";
+        for (let works of worksData) {
+          if (currentCat === works.categoryId) {
+            gallery.innerHTML += ` 
               <figure  class="${works.category.name} ${works.categoryId}">
               <img crossorigin="anonymous" src="${works.imageUrl}" alt="${works.title}">
               <figcaption>${works.title}</figcaption>
-              </figure>`
-            }
+              </figure>`;
           }
-        };
-      })
-    }
-  };
-  worksDisplay();
-const bodyIndex = document.getElementsByTagName('body')
-  const adminDisplay= async()=>{
-    await login()
-    if (info.userId === 1 ) {
-    bodyIndex.innerHTML= `
-    <div id="edit" aria-hidden="true" role="banner" >
-        <a href="#modal1" id="#modal1" class="js-modal"><i class="fa-regular fa-pen-to-square"></i>Mode édition</a>
-        <input type="submit" value="publier les changements" id="ValidModal">
-    </div>`
-    console.log(info);
-    }else{
-        alert("Vous n'avez pas les droits administrateur" )
-    }
-}
-adminDisplay()
+        }
+      }
+    });
+  }
+};
+worksDisplay();
+
+let info = localStorage.getItem("info");
+
+const bannerAdmin = document.querySelector("div");
+const loginDisplay = document.getElementById("login");
+const logoutDisplay = document.getElementById("logout");
+logoutDisplay.setAttribute("style", "display: none;");
+bannerAdmin.setAttribute("style", "display: none;");
+
+const adminDisplay = () => {
+  if (info.userId != 1) {
+    bannerAdmin.removeAttribute("style", "display: none;");
+    logoutDisplay.removeAttribute("style", "display: none");
+    loginDisplay.setAttribute("style", "display: none;");
+    // logoutDisplay.replace("display:none;", "display:null;");
+  }
+};
+
+adminDisplay();
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,36 +1,32 @@
-const login = async  ()=>{
-    await fetch("http://localhost:5678/api/users/login",{
-        // const bodyIndex = document.getElementsByTagName('body');
-       method: "POST",
-       headers: {'Content-Type': 'application/json;charset=utf-8' },
-        body: JSON.stringify({
-        email: document.getElementById("email").value,
-        password: document.getElementById("password").value, //"S0phie"
-     })
-   }) 
-   .then((response) =>response.json())
-   .then((response) =>{
-       console.log(response);
-       if(response.userId){
-           localStorage.setItem("info", JSON.stringify(response));
-           document.location.href="/FrontEnd/"
+const login = async () => {
+  await fetch("http://localhost:5678/api/users/login", {
+    // const bodyIndex = document.getElementsByTagName('body');
+    method: "POST",
+    headers: { "Content-Type": "application/json;charset=utf-8" },
+    body: JSON.stringify({
+      email: document.getElementById("email").value,
+      password: document.getElementById("password").value, //"S0phie"
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      if (response.userId) {
+        localStorage.setItem("info", JSON.stringify(response));
+        document.location.href = "/FrontEnd/";
+      } else if (response.message) {
+        alert("Mail  invalide");
+      } else {
+        alert("Mot de passe invalide");
+      }
+    });
+  console.log(body);
+  // const adminTools= document.body.createElement('div')
 
-        }
-        else if(response.message){
-            alert('Mail  invalide')
-        }
-        else{
-            alert('Mot de passe invalide')
-        }
-        
-    })
-console.log(body);
-// const adminTools= document.body.createElement('div')
+  // console.log("info");
 
-    // console.log("info");
-
-   // console.log(data);
-}
+  // console.log(data);
+};
 
 // const adminDisplay= async ()=>{
 //     await login();
