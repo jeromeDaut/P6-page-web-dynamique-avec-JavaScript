6ccaf8af18e6b1b6f598c00ad2d110c7567379f4
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-01-10 07:53:40 +0100

    rechargement du backend

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
-const helmet = require("helmet");
+// const helmet = require('helmet');
 const swaggerUi = require("swagger-ui-express");
 const yaml = require("yamljs");
 const swaggerDocs = yaml.load("swagger.yaml");
@@ -10,8 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// app.use(helmet);
+// app.use(helmet());
 app.use("/images", express.static(path.join(__dirname, "images")));
 
 const db = require("./models");
