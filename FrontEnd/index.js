6daa3e037eb71744a385a5178138a1391adc87d9
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-01-10 07:51:41 +0100

    ajout:modal2, envoyer une image dans le back, outils admin

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -11,7 +11,7 @@ logout.addEventListener("click", () => {
   localStorage.removeItem("info");
   location.href = "/FrontEnd/";
 });
-
+// display images
 const displayWorks = () => {
   gallery.innerHTML = "";
 
@@ -25,7 +25,7 @@ const displayWorks = () => {
     }
   }
 };
-
+// color filter
 const stylerFilter = () => {
   const allBtn = category.getElementsByTagName("button");
   for (let i = 0; i < allBtn.length; i++) {
@@ -35,7 +35,9 @@ const stylerFilter = () => {
     }
   }
 };
+// datafetch for buttons and gallery:
 const getData = () => {
+  // button
   fetch("http://localhost:5678/api/categories")
     .then((res) => res.json())
     .then((data) => {
@@ -61,6 +63,7 @@ const getData = () => {
       }
     })
     .catch((err) => console.log(err, "fetch error "));
+  // gallery
   fetch("http://localhost:5678/api/works")
     .then((res) => res.json())
     .then((data) => {
@@ -71,8 +74,8 @@ const getData = () => {
 };
 
 getData();
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// display banner Admin
+// -------------------------------------------------------------
+// get token and id
 let info = localStorage.getItem("info")
   ? JSON.parse(localStorage.getItem("info"))
   : null;
@@ -80,17 +83,124 @@ let info = localStorage.getItem("info")
 const bannerAdmin = document.getElementById("edit");
 const loginDisplay = document.getElementById("login");
 const logoutDisplay = document.getElementById("logout");
+const btnProject = document.querySelector(".project-btn");
 
-logoutDisplay.setAttribute("style", "display: none;");
-bannerAdmin.setAttribute("style", "display: none;");
-
+logoutDisplay.style.display = "none";
+bannerAdmin.style.display = "none";
+btnProject.style.display = "none";
+// display Admin tools
 const adminDisplay = () => {
   if (info != null) {
     bannerAdmin.removeAttribute("style", "display: none;");
     logoutDisplay.removeAttribute("style", "display: none");
+    btnProject.removeAttribute("style", "display: none");
     loginDisplay.setAttribute("style", "display: none;");
+
+    category.style.display = "none";
   }
 };
 
 adminDisplay();
-// delete works
+// =====================================================
+// =====================================================
+// MODAL
+const modalContainer = document.querySelector(".modal-container");
+const modalContainer2 = document.querySelector(".modal-container2");
+const modalBtn2 = document.querySelector(".modal-btn2");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+let currentId = 0;
+console.log(modalTriggers);
+modalTriggers.forEach((trigger) =>
+  trigger.addEventListener("click", toggleModal)
+);
+// modalTriggersTwo.forEach((triggerTwo) =>
+//   triggerTwo.addEventListener("click", toggleModal)
+// );
+modalBtn2.addEventListener("click", () => {
+  modalContainer2.classList.toggle("active");
+  modalContainer.classList.remove("active");
+});
+function toggleModal() {
+  modalContainer.classList.toggle("active");
+  modalContainer2.classList.remove("active");
+}
+
+const galleryMini = document.querySelector(".gallery-mini");
+//
+
+const displayModalGallery = () => {
+  galleryMini.innerHTML = "";
+
+  for (const modal of modalData) {
+    galleryMini.innerHTML += `
+      <figure class = "figure" >
+      <i class="fa-regular fa-trash-can recycle" data-id = " ${modal.id}"></i>
+      <img crossorigin ="anonymous" src="${modal.imageUrl}" alt="${modal.title}" data-id ="${modal.id}">
+      <a href="#modal2" class="editImg">éditer</a>
+      </figure>
+      `;
+  }
+  // if () {
+
+  // }
+};
+
+const deleteWork = (id) => {
+  fetch("http://localhost:5678/api/works/" + id, {
+    method: "DELETE",
+    headers: {
+      Authorization: "Bearer " + JSON.parse(localStorage.getItem("info")).token,
+    },
+  }).then((response) => {
+    // preventDefault();
+    console.log(response);
+    getModal();
+  });
+};
+// fetch modal
+const getModal = () => {
+  fetch("http://localhost:5678/api/works")
+    .then((res) => res.json())
+    .then((data) => {
+      modalData = data;
+      displayModalGallery();
+    })
+    .then(() => {
+      // delete individual img
+      const recycleImg = document.getElementsByClassName("recycle");
+      for (let i = 0; i < recycleImg.length; i++) {
+        recycleImg[i].addEventListener("click", () => {
+          currentId = recycleImg[i].getAttribute("data-id");
+          deleteWork(currentId);
+        });
+      }
+    })
+    .catch((err) => console.log(err, "fetch error "));
+};
+getModal();
+
+//  submit form
+const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const image = e.target.image.files[0];
+  const workTitle = e.target.workTitle.value;
+  const workCategory = e.target.workCategory.value;
+  const data = new FormData();
+  data.append("image", image);
+  data.append("workTitle", workTitle);
+  data.append("workCategory", workCategory);
+
+  fetch("http://localhost:5678/api/works", {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer " + JSON.parse(localStorage.getItem("info")).token,
+    },
+    body: data,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
