f7ae3d3177b3dcf33342528dae660a58659872f6
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-01-11 21:16:46 +0100

    ajout regex mdp/mail , alert

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,11 +1,29 @@
 const login = document.querySelector("#envoyer");
 login.addEventListener("click", () => {
+  // Get email and password values
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  // Check if email is valid
+  if (!/\S+@\S+\.\S+/.test(email)) {
+    alert("Adresse email non valide");
+    return;
+  }
+
+  // Check if password is valid
+  if (!/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/.test(password)) {
+    alert(
+      "Mot de passe non valide. Doit contenir au moins 6 caractères, une majuscule et un chiffre"
+    );
+    return;
+  }
+
   fetch("http://localhost:5678/api/users/login", {
     method: "POST",
     headers: { "Content-Type": "application/json;charset=utf-8" },
     body: JSON.stringify({
-      email: document.getElementById("email").value,
-      password: document.getElementById("password").value, //"S0phie"
+      email: email,
+      password: password,
     }),
   })
     .then((response) => response.json())
@@ -19,6 +37,6 @@ login.addEventListener("click", () => {
       } else {
         alert("Mot de passe invalide");
       }
-    });
+    }).catch;
   console.log(body);
 });
